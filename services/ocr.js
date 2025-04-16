b56892d9856a5216a9b17bb36991184a6309376f
santi1522 <santiagogomez152@gmail.com>
2025-04-16 00:42:58 -0300

    Update ocr.js

--- services/ocr.js
+++ services/ocr.js
@@ -2,8 +2,12 @@ const Tesseract = require("tesseract.js");
 const path = require("path");
 
 async function extractText(imagePath) {
-  const result = await Tesseract.recognize(path.resolve(imagePath), "spa");
-  return { raw_text: result.data.text };
+  const result = await Tesseract.recognize(path.resolve(imagePath), "spa", {
+    logger: m => console.log("[OCR]", m.status)
+  });
+  return {
+    raw_text: result.data.text
+  };
 }
 
 module.exports = { extractText };
